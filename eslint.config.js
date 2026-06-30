dd6237f96c690bd4b1ab171441531c3223d0eae1
Kaan <kaan@jotform.com>
2026-06-30 10:05:51 +0300

    fix: move prepublishOnly out of package exports
    Invalid non-dot key in the exports map triggered a Storybook/esbuild warning.
    Also bumps Storybook via upgrade and patch version to 0.1.2.

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,27 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 import babelParser from '@babel/eslint-parser'
 import reactPlugin from 'eslint-plugin-react'
 
-export default [
-  {
-    files: ['src/**/*.{js,jsx}'],
-    languageOptions: {
-      parser: babelParser,
-      parserOptions: {
-        requireConfigFile: false,
-        babelOptions: {
-          presets: ['@babel/preset-react'],
-        },
+export default [{
+  files: ['src/**/*.{js,jsx}'],
+  languageOptions: {
+    parser: babelParser,
+    parserOptions: {
+      requireConfigFile: false,
+      babelOptions: {
+        presets: ['@babel/preset-react'],
       },
-      ecmaVersion: 'latest',
-      sourceType: 'module',
     },
-    plugins: {
-      react: reactPlugin,
-    },
-    rules: {
-      semi: ['error', 'always'],
-      'no-extra-semi': 'error',
-    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
-]
+  plugins: {
+    react: reactPlugin,
+  },
+  rules: {
+    semi: ['error', 'always'],
+    'no-extra-semi': 'error',
+  },
+}, ...storybook.configs["flat/recommended"]];
